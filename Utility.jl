33453298d13782a630e49dd34db56cf057f8f065
salsaif <sis.7.7.93@gmail.com>
2016-11-29 21:40:05 -0800

    Created a function that calculates the utility of an agent

---
+++ Utility.jl
@@ -0,0 +1,30 @@
+using LightGraphs
+using StatsBase
+
+type Agent
+    loc::Int64
+    cov::Int64
+    com::Int64
+end
+function Utility(Agent, Agents, g)
+  others = setdiff(Agents, Agent)
+  commons = []
+  for other in others
+    if gdistances(g,Agent.loc)[other.loc] <= other.com
+        commons = union(commons, neighborhood(g, Agent.loc, Agent.cov), neighborhood(g, other.loc, other.cov))
+    end
+  end
+  score = length(setdiff(neighborhood(g, Agent.loc, Agent.cov), commons))
+  return score
+end
+
+N = 12
+n = 2
+g = barabasi_albert(N,1)
+Agents = []
+
+# AgentsLoc = sample(1:N, n, replace = false)
+# AgentsCoverage = Dict()
+# for agent in AgentsLoc
+#   AgentsCoverage[agent] = neighbors(g,agent)
+# end

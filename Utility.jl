51e7f0db55420ad020b02fe07e80d298083bae48
salsaif <sis.7.7.93@gmail.com>
2016-11-30 22:44:00 -0800

    Update Utility.jl

--- Utility.jl
+++ Utility.jl
@@ -7,18 +7,27 @@ type Agent
     com::Int64
 end
 function Utility(Agent, Agents, g)
-  others = setdiff(Agents, Agent)
-  commons = []
+  others = setdiff(Agents, [Agent])
+  @show others
+  coverage = neighborhood(g, Agent.loc, Agent.cov)
+  Common = []
   for other in others
     if gdistances(g,Agent.loc)[other.loc] <= other.com
-        commons = union(commons, neighborhood(g, Agent.loc, Agent.cov), neighborhood(g, other.loc, other.cov))
+        CovIntersection = intersect(coverage, neighborhood(g, other.loc, other.cov))
+        Common = union(Common, CovIntersection)
+        @show Common
     end
   end
-  score = length(setdiff(neighborhood(g, Agent.loc, Agent.cov), commons))
+  score = length(setdiff(coverage, Common))
   return score
 end
 
-# N = 12
-# n = 2
-# g = barabasi_albert(N,1)
-# Agents = []
+N = 12
+n = 2
+g = barabasi_albert(N,1)
+Agents = [Agent(1,1,4), Agent(4,1,2), Agent(6,2,3)]
+for agent in Agents
+  @show agent.loc
+  @show neighborhood(g, agent.loc, agent.cov)
+  @show Utility(agent, Agents, g)
+end
